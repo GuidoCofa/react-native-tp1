969918b38d8bf3eda70fcd48c1af8ed9ba15d352
Android Studio <devandroid@ort>
2024-07-29 12:01:38 -0300

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>polshetta te suelto la hand</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,83 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+import * as SplashScreen from 'expo-splash-screen';
+
+
+export default function App() {
+
+  const [isLoading, setIsLoading] = useState(true);
+
+
+  useEffect(() => {
+
+      const esperarAlgunosMilisegundos = async () => {
+
+      // Evitar que el splash screen se oculte automáticamente
+
+      await SplashScreen.preventAutoHideAsync();
+
+      try {
+
+          // Acá hacemos todas las llamadas asincrónicas que necesitamos.
+
+          // Por ejemplo precarga de Fonts, llamadas a APIs externas, etc...
+
+         
+
+          // Simular una demora de 5 segundos.
+
+          await new Promise(resolve => setTimeout(resolve, 5000));
+
+      } catch (e) {
+
+          console.warn(e);
+
+      } finally {
+
+          // Cambiar el estado de carga a falso y ocultar el splash screen
+
+          setIsLoading(false);
+
+          await SplashScreen.hideAsync();
+
+      }
+
+    };
+
+
+    esperarAlgunosMilisegundos();
+
+  }, []);
+
+
+  // Renderizado condicional:
+
+  // Mostrar una pantalla en blanco mientras se carga
+
+  if (isLoading) {
+
+    return null;
+
+  }
+
+
+  return (
+
+    <View style={styles.container}>
+
+      <StatusBar style="auto" />
+
+      <Text>Open up App.js to start working in your app!</Text>
+
+    </View>
+
+  );
+
+}
